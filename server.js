0655f47d8acac2bf35ba8006043449d585bc5617
Rahul Kumar <rahulsinghaniya1709@gmail.com>
2024-06-29 16:12:10 +0530

    updated bg

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const socket = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 app.use(cors({
-    origin : ['http://localhost:3000','http://localhost:3001'],
+    origin : ['http://localhost:3000','http://localhost:3001','https://master--moonlit-moonbeam-e6c303.netlify.app'],
     credentials: true
 }))
 
